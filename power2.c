b233e98b434fa525e50ab6c11c1b090724fa7768
Mihir Chakma <91591055+mihirchakma@users.noreply.github.com>
2023-07-09 10:02:51 +0530

    Add files via upload

---
+++ power2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define N 16
+
+int main(void) {
+  int n;
+  int val = 1;
+
+  printf("\t  n  \t    2^n\n");
+  printf("\t================\n");
+  for (n=0; n<=N; n++) {
+    printf("\t%3d \t %6d\n", n, val); 
+    val = 2*val;
+  }
+  return 0;
+}
